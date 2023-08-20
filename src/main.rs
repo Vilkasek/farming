4ff9473782f3107f1a1b650ed1c2b0595699f907
Jakub Brandt <tv.brandon.nr1@gmail.com>
2023-08-20 17:26:22 +0200

    Initial commit after learning

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,6 @@
 use bevy::prelude::*;
 
-fn main() 
+fn main()
 {
-    App::new()
-        .add_systems(Startup, add_people)
-        .add_systems(Update, greet_people)
-        .run();
+    
 }
-
-fn add_people(mut commands: Commands)
-{
-    commands.spawn((Person, Name("Elaina".to_string())));
-    commands.spawn((Person, Name("Dril".to_string())));
-    commands.spawn((Person, Name("Torin".to_string())));
-}
-
-fn greet_people(query: Query<&Name, With<Person>>)
-{
-    for name in &query
-    {
-        println!("Hello {}!", name.0);
-    }
-}
-
-#[derive(Component)]
-struct Person;
-
-#[derive(Component)]
-struct Name(String);
